a3aa69499f66be6e630075ecb1b815dfa68f55ce
MAvila5 <61132417+MAvila5@users.noreply.github.com>
2024-02-07 20:15:23 -0500

    Updated layout.js

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,39 +1,30 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-import { BackendURL } from "./component/backendURL";
-
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./component/ScrollToTop";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./views/HomePage/HomePage";
+import ServicesPage from "./views/ServicesPage/ServicesPage";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
-
-//create your first component
+// Create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    // the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename in the .env file located at the root of this project, E.g: BASENAME=/my-app/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
-
     return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
+        <Router basename={basename}>
+            <ScrollToTop>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/services" element={<ServicesPage />} />
+                    <Route path="*" element={<h1>Not found!</h1>} />
+                </Routes>
+                <Footer />
+            </ScrollToTop>
+        </Router>
     );
 };
 
