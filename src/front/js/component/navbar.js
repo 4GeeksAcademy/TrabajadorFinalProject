90bf9b4c0d34ae5ad699de2124fff8737f0f6bcb
MAvila5 <61132417+MAvila5@users.noreply.github.com>
2024-02-07 20:07:08 -0500

    Updated navbar.js

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/Navbar.css';
+import logo from '../../img/Trabajador.com_Logo.png';
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <Link className="navbar-brand" to="/">
+        <img src={logo} alt="Trabajador Logo" />
+      </Link>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/about-us">About Us</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/reviews">Reviews</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/contact-us">Contact Us</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
+
+export default Navbar;
