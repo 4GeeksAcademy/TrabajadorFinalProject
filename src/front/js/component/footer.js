ca2902effd60c5005fa42f6324e9d0673ab966cd
MAvila5 <61132417+MAvila5@users.noreply.github.com>
2024-02-07 20:06:33 -0500

    Updated footer.js

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,37 @@
-import React, { Component } from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../img/Trabajador.com_Logo.png';
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+const Footer = () => {
+  return (
+    <footer className="container">
+      <div className="row">
+        <div className="col">
+          <img src={logo} alt="Trabajador Logo" />
+          <p>Trabajador.com</p>
+          {/* Social media links */}
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </div>
+        <div className="col">
+          <h5>Quick Links</h5>
+          <Link to="/">Home</Link>
+          <Link to="/about-us">About Us</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/reviews">Reviews</Link>
+          <Link to="/contact-us">Contact Us</Link>
+        </div>
+        <div className="col">
+          <h5>Top Company Partners</h5>
+          <p>Compania Uno</p>
+          <p>Empresa Dos</p>
+          <p>Negocio Tres</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
