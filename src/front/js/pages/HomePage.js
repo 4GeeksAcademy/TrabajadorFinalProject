c6d2355f7c6575f767e9a61bd74e931f53e426dc
MAvila5 <61132417+MAvila5@users.noreply.github.com>
2024-03-02 01:53:39 +0000

    Added ServicesOffered.js

--- src/front/js/pages/HomePage.js
+++ src/front/js/pages/HomePage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Jumbotron from '../components/Jumbotron.js';
 import AboutUs from '../components/AboutUs.js';
-import ServiceCard from '../components/ServiceCard.js';
-import CompanyPartnersCarousel from '../components/CompanyPartnersCarousel.js';
 import Carousel from '../components/Carousel.js'
+import ServicesOffered from '../components/ServicesOffered.js'
+import CompanyPartnersCarousel from '../components/CompanyPartnersCarousel.js';
 
 
 const HomePage = () => {
@@ -15,9 +15,8 @@ const HomePage = () => {
         <h2>Meet Our Featured Trabajadores!</h2>
         <Carousel />
       </section>
-      <section className="service-cards">
-        <h2>Services Offered</h2>
-        <ServiceCard />
+      <section className="service-icons">
+        <ServicesOffered />
       </section>
       <section className="company-partners">
         <h2>Company Partners</h2>
