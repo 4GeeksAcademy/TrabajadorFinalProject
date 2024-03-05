75ef7a29cf45e85d3ec69a4b3c8b2a22999d5b63
Shane B <dotfortun@users.noreply.github.com>
2024-03-05 00:10:26 +0000

    Added to make a unique commit.

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -4,6 +4,7 @@ import React, { useContext, useState } from 'react';
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 
+// Hello, world!
 
 
 export const Login = () => {
