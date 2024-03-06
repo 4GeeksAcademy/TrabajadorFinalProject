8249f55ba9730e5debe4e27af5c665f03ced2584
JoseR4Geeks <143768985+JoseR4Geeks@users.noreply.github.com>
2024-03-06 21:51:34 +0000

    fixed login css

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,19 +1,13 @@
-import '../../styles/Login.css';
-
 import React, { useContext, useState } from 'react';
 import { Context } from "../store/appContext";
-// import { useHistory } from "react-router";
-
-
+import styles from '../../styles/Login.module.css'; // Import CSS module
 
 export const Login = () => {
   const { store, actions } = useContext(Context);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  // const history = useHistory();
   const token = sessionStorage.getItem("token");
   console.log("This is your token", token)
-
 
   const handleClick = () => {
     actions.login(email, password).then(() => {
@@ -22,30 +16,30 @@ export const Login = () => {
   };
 
   return (
-    <div className="container">
-      <div className="login">
+    <div className={styles.container}>
+      <div className={styles.login}>
         <h2>Login</h2>
-        {(token && token != "" && token != undefined) ? "You are logged in with this token" + token :
-
+        {(token && token !== "" && token !== undefined) ? `You are logged in with this token ${token}` :
           <div>
             <input
+              className={styles.input}
               type="text"
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)} />
             <input
+              className={styles.input}
               type="password"
-              placeholder="password"
+              placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <button onClick={handleClick}>Login</button>
+            <button className={styles.button} onClick={handleClick}>Login</button>
           </div>
-
-          // {message && <p className="message">{setMessage}</p>}
         }
       </div>
     </div>
   )
 }
+
 export default Login;
