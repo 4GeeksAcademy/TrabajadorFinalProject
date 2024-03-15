ba8b76d212439e64c85f195bcf18e91e99c47ff5
MAvila5 <61132417+MAvila5@users.noreply.github.com>
2024-03-15 21:17:07 +0000

    About Us Section Edits

--- src/front/js/components/AboutUsSection.js
+++ src/front/js/components/AboutUsSection.js
@@ -9,7 +9,7 @@ const AboutUsSection = () => {
                 <div className="row align-items-center">
                     <div className="col-md-6 text-section">
                         <h2>Empower Your Vision: Let Trabajadores Craft Your Future!</h2>
-                        <p>At Trabajador.com, we're more than just a platform; we're a community-driven by the passion to bring local coding professionals and businesses together. Born out of the need to promote local talent in the ever-evolving digital landscape, we provide a unique space where coding service providers can showcase their skills to a local audience. Our dedication to fostering local connections not only helps businesses find the talent they need but also supports the growth of local economies. By choosing Trabajador.com, you're not just hiring a freelancer; you're investing in your community.</p>
+                        <p>Trabajador uplifts communities by uniting local coding experts with businesses, championing the growth of regional talent. Our vibrant platform celebrates and elevates regional economies, creating opportunities for skills to shine and partnerships to flourish. Opting for Trabajador means you're not just hiring; you're positively empowering your community with every local engagement.</p>
                         <button className="btn button-62 button-62-primary">Learn more about Trabajadores</button>
                     </div>
                     <div className="col-md-6 image-section">
