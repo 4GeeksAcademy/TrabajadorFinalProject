07da276198f2772acbb834fbc8dc4cce4ce9806c
BrodyDubach <117411713+BrodyDubach@users.noreply.github.com>
2024-03-22 19:12:17 +0000

    profile page is now working(I wanted to save these changes before trying to make it look nice)

--- src/front/js/pages/Profile.js
+++ src/front/js/pages/Profile.js
@@ -17,9 +17,44 @@ const Profile = () => {
     }, [])
 
     return <>
-        <div>
-            <h1>{store.user?.email}</h1>
-        </div>
+          <div className="container">
+    <div style={{ width: "100%", postion: "inline-block" }}>
+      <img
+        src="https://www.quantico.marines.mil/portals/147/Templates/Under%20Construction.jpg?ver=AEqzBgQL0jaMD0aQbmwkMg%3D%3D"
+        style={{ height: "300px", width: "100%" }}
+      ></img>
+      {/* <img
+        className="profilePic"
+        // src="https://static.vecteezy.com/system/resources/thumbnails/030/504/836/small/avatar-account-flat-isolated-on-transparent-background-for-graphic-and-web-design-default-social-media-profile-photo-symbol-profile-and-people-silhouette-user-icon-vector.jpg"
+        style={{
+          height: "200px",
+          width: "230px",
+          right: "43.5%",
+          objectFit: "cover",
+          top: "15%",
+          position: "absolute",
+        }}
+      ></img> */}
+    </div>
+    <h2 style={{
+          right: "40%",
+          top: "45%",
+          position: "absolute",
+        }}>Check back soon!</h2>
+        <Link to="/previous-orders">Order History</Link>
+        <span style={{ margin: "0 16px" }}>|</span>
+        <Link to="/current-orders">Order Tracking</Link>
+        <span style={{ margin: "0 16px" }}>|</span>
+        <Link to="/current-orders">Review History</Link>
+        <span style={{ margin: "0 16px" }}>|</span>
+        <Link to="/current-orders">Profile Information</Link>
+        <span style={{ margin: "0 16px" }}>|</span>
+        <Link to="/current-orders">Become a Vendor</Link>
+        <span style={{ margin: "0 16px" }}>|</span>
+        <Link to="/current-orders">Customer Support</Link>
+   
+    </div>
+  
     </>
 }
 
